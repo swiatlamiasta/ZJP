16ab4cca77d79cf867eec64bb2c1c683fbe055a7
Artur Golebiewski <artgolebiewski@gmail.com>
2019-01-08 21:29:59 +0100

    conditional replaced with polymorphism

--- gilded_rose.py
+++ gilded_rose.py
@@ -12,41 +12,15 @@ class GildedRose(object):
 
     def update_one_item(self, item):
         if item.name == "Aged Brie":
-            self.increment_quality(item)
-            item.sell_in = item.sell_in - 1
-            if self.has_expired(item):
-                self.increment_quality(item)
-                item.sell_in = item.sell_in - 1
-        elif item.name == "Backstage passes to a TAFKAL80ETC concert":
-            self.increment_quality(item)
-            item.sell_in = item.sell_in - 1
-            if item.sell_in < 11:
-                self.increment_quality(item)
-            if item.sell_in < 6:
-                self.increment_quality(item)
-            if self.has_expired(item):
-                item.quality = 0
+            item.__class__ = Brie
         elif item.name == "Sulfuras, Hand of Ragnaros":
-            pass
-        else:
-            self.decrement_quality(item)
-            item.sell_in = item.sell_in - 1
-            if self.has_expired(item):
-                self.decrement_quality(item)
+            item.__class__ = Sulfuras
+        elif item.name == "Backstage passes to a TAFKAL80ETC concert":
+            item.__class__ = BackstagePass
+        elif item.name == "Conjured Mana Cake":
+            item.__class__ = Conjured
+        item.update(item)
 
-
-    def has_expired(self, item):
-        return item.sell_in < 0
-
-
-    def decrement_quality(self, item):
-        if item.quality > 0:
-            item.quality = item.quality - 1
-
-
-    def increment_quality(self, item):
-        if item.quality < 50:
-            item.quality = item.quality + 1
 
 
 class Item:
@@ -57,3 +31,55 @@ class Item:
 
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+
+    def has_expired(self, item):
+        return item.sell_in < 0
+
+    def decrement_quality(self, item):
+        if item.quality > 0:
+            item.quality = item.quality - 1
+
+    def increment_quality(self, item):
+        if item.quality < 50:
+            item.quality = item.quality + 1
+
+    def update(self, item):
+        self.decrement_quality(item)
+        item.sell_in = item.sell_in - 1
+        if self.has_expired(item):
+            self.decrement_quality(item)
+
+
+class Brie(Item):
+
+    def update(self, item):
+        self.increment_quality(item)
+        item.sell_in = item.sell_in - 1
+        if self.has_expired(item):
+            self.increment_quality(item)
+            item.sell_in = item.sell_in - 1
+
+
+class BackstagePass(Item):
+
+    def update(self, item):
+        self.increment_quality(item)
+        item.sell_in = item.sell_in - 1
+        if item.sell_in < 11:
+            self.increment_quality(item)
+        if item.sell_in < 6:
+            self.increment_quality(item)
+        if self.has_expired(item):
+            item.quality = 0
+
+
+class Sulfuras(Item):
+
+    def update(self, item):
+        pass
+
+
+class Conjured(Item):
+    def decrement_quality(self, item):
+        if item.quality > 0:
+            item.quality = item.quality - 2
